d0c215ca3a9953c9a4839b512c615d6c3b599745
simon.chen <lovelife0314@163.com>
2018-11-06 09:10:31 +0800

    commit from mbp

--- ReportGenenrator.py
+++ ReportGenenrator.py
@@ -14,10 +14,10 @@ VERDICT_TEXT = r' (supported|not supported|present|not present or not active|dis
 
 # connect to template excel
 def template_open():
-    global expectResult, realResult
+    global expectTemplate, realTemplate
     try:
-        expectResult = load_workbook('template_RGpath.xlsx')
-        realResult = load_workbook('template_realResult.xlsx')
+        expectTemplate = load_workbook('template_RGpath.xlsx')
+        realTemplate = load_workbook('template_realResult.xlsx')
     except IOError as e:
         raw_input(e)
         exit()
@@ -90,7 +90,7 @@ def load_data_from_pdf(pdf):
 
 # Generate results for sub-condition
 def ics_static_save(raw):
-    temp_sheet = expectResult['ICS_Config_Static'] # list all possible configurtion
+    temp_sheet = expectTemplate['ICS_Config_Static'] # list all possible configurtion
     for key in temp_sheet['B']:
         if key.value is None:
             verdict = 'True'
@@ -110,20 +110,26 @@ def ics_static_save(raw):
             verdict = eval(verdict[:-4])
         else:
             verdict = raw[key.value] if key.value != "MSD Support" else (not raw[key.value])
+            if key.value == 'Max Dynamic Reader Limit sets supported' and temp_sheet['C15'].value == 'False':
+                verdict = 'False'
         if verdict is None:
             raise Exception('There is blank to be filled at row %s. Please retry after fill it.'%key.row)
         temp_sheet['C' + str(key.row)] = str(verdict)
 
 def gen_expect_result():
-    temp_sheet = expectResult['ICS_Config_Static'] # list all possible configurtion
-    for path_sheet in [expectResult['MSD Path(Online Only)'], expectResult['MSD Path(Online Capable)'], expectResult['qVSDC Path']]:
+    icsFile = raw_input('Please input the path of decrypted ICS doc.\n')
+    icsData = load_data_from_pdf(icsFile)
+    ics_static_save(icsData)
+
+    temp_sheet = expectTemplate['ICS_Config_Static'] # list all possible configurtion
+    for path_sheet in [expectTemplate['MSD Path(Online Only)'], expectTemplate['MSD Path(Online Capable)'], expectTemplate['qVSDC Path']]:
         for condition_cell in path_sheet['B']:
             condition = condition_cell.value  # get condition description from template
             #outcome = ''  # contain logic method and verdict calculation the final result
             if condition not in [None, 'CONDITIONS ']:
-                if (temp_sheet['C2'].value == 'True' and path_sheet in [expectResult['MSD Path(Online Only)'], expectResult['MSD Path(Online Capable)']])\
-                        or (temp_sheet['C3'].value == 'True' and path_sheet == expectResult['MSD Path(Online Only)'])\
-                        or (temp_sheet['C4'].value == 'True' and path_sheet == expectResult['MSD Path(Online Capable)']):
+                if (temp_sheet['C2'].value == 'True' and path_sheet in [expectTemplate['MSD Path(Online Only)'], expectTemplate['MSD Path(Online Capable)']])\
+                        or (temp_sheet['C3'].value == 'True' and path_sheet == expectTemplate['MSD Path(Online Only)'])\
+                        or (temp_sheet['C4'].value == 'True' and path_sheet == expectTemplate['MSD Path(Online Capable)']):
                     condition = False
                 else:
                     # format conditions
@@ -163,27 +169,119 @@ def gen_expect_result():
                     subcondition = True if path_sheet[subcondition + str(condition_cell.row)].value == 'Y' else False
                     if path_sheet[subcondition_area[index] + '1'].value == 'qVSDC/MSD active mode' and temp_sheet['C31'].value == 'False':
                         subcondition = False
-                    path_sheet[result_area[index] + str(condition_cell.row)].value = 'PASS' if subcondition and condition else 'N/A'
-    expectResult.save('expectResult.xlsx')
-    expectResult.close()
+                    path_sheet[result_area[index] + str(condition_cell.row)].value = 'Pass' if subcondition and condition else 'NA'
+    expectTemplate.save('expectResult.xlsx')
+    expectTemplate.close()
+    realTemplate.close()
+
+
+def load_results_from_icstool(input, path_sheet, col):
+    #results = {}
+    col = chr(ord(col) + 1)
+    try:
+        icc_workbook = load_workbook(input)
+        icc_results = icc_workbook['Test Results']
+    except IOError as e:
+        raw_input(e)
+        exit()
+    # for tc in icc_results['B']:
+    #     if tc.value not in [None, 'TestCase'] and icc_results['C' + str(tc.row)].value != 'N/A' and (tc.value not in results or results[tc.value] != 'Fail'):
+    #         results[tc.value] = icc_results['C' + str(tc.row)].value
+    for tc_real in path_sheet['A']:
+        if tc_real.value not in [None, 'TEST CASES']:
+            tc_realv = tc_real.value.replace('.', '')
+            for tc_icc in icc_results['A']:
+                visaTCname = icc_results['B' + str(tc_icc.row)].value.split('.')[0]
+                tc_iccv = tc_icc.value.replace('NO SCRIPT', visaTCname)
+                if re.search(tc_realv, tc_iccv) and icc_results['C' + str(tc_icc.row)].value != 'N/A' and path_sheet[col + str(tc_real.row)].value != 'Fail':
+                    path_sheet[col + str(tc_real.row)].value = icc_results['C' + str(tc_icc.row)].value
+            if not path_sheet[col + str(tc_real.row)].value or path_sheet[col + str(tc_real.row)].value.strip() == '':
+                path_sheet[col + str(tc_real.row)].value = 'NA'
+    icc_workbook.close()
+
 
 def gen_real_result():
-    titles = [t.value for t in realResult['Titles']['A']]  # list all possible titles
-    f = [f.value for f in realResult['Titles']['B']]
-    for path_sheet in [realResult['MSD Path(Online Only)'], realResult['MSD Path(Online Capable)'], realResult['qVSDC Path']]:
+    Filepath = getcwd()
+    #titles = [t.value for t in realTemplate['Titles']['A']]  # list all possible titles
+    #f = [f.value for f in realTemplate['Titles']['B']]
+    for path_sheet in [realTemplate['MSD Path(Online Only)'], realTemplate['MSD Path(Online Capable)'], realTemplate['qVSDC Path']]:
         for title in path_sheet['1']:
-            if title.value in titles:
-                f_1 = [f[i] for i,v in enumerate(titles)]
-                raw_input(f_1)
+            FilenameCount = [] # initial fine name counter
+            if title.value is not None:
+                title_index = [f.row for f in realTemplate['Titles']['A'] if (path_sheet.title + title.value) == f.value]
+                if len(title_index) == 1:
+                    finder = realTemplate['Titles']['B' + str(title_index[0])].value
+                    for Filename in listdir(Filepath):
+                        if re.search(r'^(?!.~).*' + str(finder), Filename):
+                            FilenameCount.append(Filename)
+                    if len(FilenameCount) == 0:
+                        for tc_real in path_sheet['A']:
+                            if tc_real.value not in [None, 'TEST CASES']:
+                                path_sheet[chr(ord(title.column) + 1) + str(tc_real.row)].value = 'NA'
+                    elif len(FilenameCount) == 1:
+                        load_results_from_icstool(FilenameCount[0], path_sheet, title.column)#write according file
+                    elif len(FilenameCount) > 1:
+                        raise Exception('There are more than one ICC solution excels which share the same name!')
+                elif len(title_index) > 1:
+                    raise Exception('Shall not appear the same value in "Titles" worksheet!')
+    realTemplate.save('realResult.xlsx')
+    realTemplate.close()
+    expectTemplate.close()
 
 
-    # Filepath = getcwd()
-    # for Filename in listdir(Filepath):
-    #     Filename = re.sub(r'\.\w+', '', Filename)
-    #     print Filename
+def cmp_results():
+    try:
+        expectResult = load_workbook('expectResult.xlsx')
+        realResult = load_workbook('realResult.xlsx')
+    except IOError as e:
+        raw_input(e)
+        exit()
+    for pathsheet_real in [realResult['MSD Path(Online Only)'], realResult['MSD Path(Online Capable)'], realResult['qVSDC Path']]:
+        result_area = [res.column for res in pathsheet_real['2'] if res.value == 'RESULT']
+        for resultCol in result_area:
+            for resultRow in range(3, pathsheet_real.max_row):
+                cmpReal = pathsheet_real[str(resultCol) + str(resultRow)].value
+                cmpExpect = expectResult[pathsheet_real.title][str(resultCol) + str(resultRow)].value
+                if cmpReal != cmpExpect:
+                    pathsheet_real[str(resultCol) + str(resultRow)].fill = STYLE_RED
+    realResult.save('realResult.xlsx')
+    realResult.close()
+    expectResult.close()
 
-template_open()
-# ics = load_data_from_pdf('out1.pdf')
-# ics_static_save(ics)
-# gen_expect_result()
-gen_real_result()
+def user_interface():
+    option = raw_input('***  Which service do you prefer  ***\n1. Generate expectResult from decrypted ICS doc.\n'
+                       '2. Generate realResult from ICC solution tool.\n'
+                       '3. Compare expectResult and realResult(you may need to perform service 1 and 2 first)\n'
+                       '4. Perform all above services.\n'
+                       '5. Exit\n')
+    if option == '1':
+        template_open()
+        gen_expect_result()
+        return user_interface()
+    elif option == '2':
+        template_open()
+        gen_real_result()
+        return user_interface()
+    elif option == '3':
+        cmp_results()
+        return user_interface()
+    elif option == '4':
+        template_open()
+        gen_expect_result()
+        template_open()
+        gen_real_result()
+        cmp_results()
+        return user_interface()
+    elif option == '5':
+        exit()
+    else:
+        print('Please enter correct service index number.')
+        return user_interface()
+    # ics = load_data_from_pdf('out1.pdf')
+    # ics_static_save(ics)
+    # gen_expect_result()
+    # gen_real_result()
+    # cmp_results()
+
+if __name__ == '__main__':
+    user_interface()
